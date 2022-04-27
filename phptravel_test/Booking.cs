5e8d71a13661dac51e93b5ce323bcabc95d644f6
Nguyễn Long <long.nguyenhabao@gmail.com>
2022-04-27 14:25:42 +0700

    split booking page and updated code

---
+++ phptravel_test/Booking.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Safari;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace phptravel_test
+{
+    public class Bookings
+    {
+        public void mainPage(IWebDriver driver)
+        {
+            // Find booking section and click to fo to booking page
+            IWebElement clickBookings = driver.FindElement(By.XPath("/html/body/nav/div/div/ul/li[2]"));
+            clickBookings.Click();
+
+            // Create a wait method to wait the page move to booking page successful after clicking
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            Func<IWebDriver, bool> waitForElement = new Func<IWebDriver, bool>((IWebDriver Web) =>
+            {
+                return Web.Title == "All Bookings View";
+            });
+            wait.Until(waitForElement);
+
+            // Check whether the Booking page is displayed
+            string currentWindowTitle = "All Bookings View";
+            Assert.AreEqual(currentWindowTitle, driver.Title);
+        }
+
+        public void verifyElem(IWebDriver driver)
+        {
+            // Find and verify Show entries menu has default value is 10
+            int entrDefaultValue = 10;
+            IWebElement showEntries = driver.FindElement(By.XPath("//div[@class='dataTables_length']//child::select"));
+            String actualDefault = showEntries.GetAttribute("value");
+            Assert.True(!String.IsNullOrEmpty(actualDefault));
+
+            int value = Int32.Parse(actualDefault);
+            Assert.AreEqual(entrDefaultValue, value);
+
+            // Find and verify seach textbox has the default value is empty
+            IWebElement searchBar = driver.FindElement(By.XPath("//div[@class='dataTables_filter']//child::input"));
+            String searchValue = showEntries.GetAttribute("placeholder");
+            Assert.True(String.IsNullOrEmpty(searchValue));
+
+            // Find the data table in booking page and verify it contains the required columns
+            var listColumns = new string[] {"#","ID","Ref","Customer","Module","Supplier","From","Date","Price","Pnr","Booking Status","Payment Status","Execute Booking"};
+            ReadOnlyCollection<IWebElement> bookingTable = driver.FindElements(By.XPath("//table[@id='data']//child::tr//th"));
+            List<string>actualListColumns = bookingTable.Select(o => o.Text.Trim()).Where(o => !String.IsNullOrEmpty(o)).ToList();
+            Assert.True(listColumns.SequenceEqual(actualListColumns));
+        }
+    }
+}
--- phptravel_test/Dashboard.cs
+++ phptravel_test/Dashboard.cs
@@ -19,49 +19,23 @@ namespace phptravel_test
         // Create a setup process to access into phptravels page
         IWebDriver driver;
         private Auth auth = new Auth();
+        private Bookings booking = new Bookings();
+
         [SetUp]
         public void startBrowser()
         {
             driver = new SafariDriver();
         }
 
-        // This is the main testing processes where we write methods for automation testing
         [Test()]
         public void Dashboard()
         {
-            // Go to the phptravels page
             auth.doLogin(driver);
             verifyItems();
-            settingDropdownMenu();
-            bookingPage();
+            menuSettings();
+            booking.mainPage(driver);
+            booking.verifyElem(driver);
         }
-
-        //void doLogin()
-        //{
-        //    // Find the email field and assign the value to it
-        //    IWebElement email = driver.FindElement(By.Name("email"));
-        //    email.SendKeys("admin@phptravels.com");
-
-        //    // Find the password field and assign the value to it
-        //    IWebElement password = driver.FindElement(By.Name("password"));
-        //    password.SendKeys("demoadmin");
-
-        //    // Find the submit button and click after entered the email and password to log in
-        //    IWebElement pressLogin = driver.FindElement(By.XPath("//button[@type = 'submit']"));
-        //    pressLogin.Click();
-
-        //    // Create a wait method to wait the logging in process finishes successfully and move to Dashboard page
-        //    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        //    Func<IWebDriver, bool> waitForElement = new Func<IWebDriver, bool>((IWebDriver Web) =>
-        //    {
-        //        return Web.Title == "Dashboard";
-        //    });
-        //    wait.Until(waitForElement);
-
-        //    // Check whether the Dashboad page is displayed
-        //    string currentWindowTitle = "Dashboard";
-        //    Assert.AreEqual(currentWindowTitle, driver.Title);
-        //}
 
         void verifyItems()
         {
@@ -81,64 +55,25 @@ namespace phptravel_test
             Assert.True(expListIcons.SequenceEqual(dashIcon));
         }
 
-        void settingDropdownMenu()
+        void menuSettings()
         {
             // Increase the window size
             driver.Manage().Window.Maximize();
-            // Find the setting section with its descendant and store as a collection
-            ReadOnlyCollection<IWebElement> findSettings = driver.FindElements(By.XPath("//*[@id='collapseDashboards']//descendant::a"));
 
+            // Create expectation list of menu settings dropdown
+            var expectSettingsList = new string[] { "General Settings", "Updates", "Currencies", "Payment Gateways", "Social Connections", "Homepage Sliders", "Email Templates", "SMS API Settings", "BackUp", "Ban IP" };
+            // Find the setting section and get all the options inside the menu to verify those are available
+            ReadOnlyCollection<IWebElement> menuSettings = driver.FindElements(By.XPath("//div[@id='collapseDashboards']//child::a"));
+            List<string> actualSettingsList = menuSettings.Select(o => o.Text.Trim()).Where(o => !String.IsNullOrEmpty(o)).ToList();
+            Assert.True(expectSettingsList.SequenceEqual(actualSettingsList));
 
-            // Check whether we can get all the options inside settings
-            foreach (var item in findSettings)
+            // Verify to ensure there is no null or empty options. Then whether it's clickable
+            foreach (var item in menuSettings)
             {
-                // Check whether the option is null or empty. Then whether it's clickable
                 var url = item.GetAttribute("href");
                 Assert.True(!String.IsNullOrEmpty(url));
                 Assert.True(item.Enabled);
             }
-        }
-
-        void bookingPage()
-        {
-            // Find booking section and click to fo to booking page
-            IWebElement clickBooking = driver.FindElement(By.XPath("/html/body/nav/div/div/ul/li[2]"));
-            clickBooking.Click();
-
-            // Create a wait method to wait the page move to booking page successful after clicking
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            Func<IWebDriver, bool> waitForElement = new Func<IWebDriver, bool>((IWebDriver Web) =>
-            {
-                return Web.Title == "All Bookings View";
-            });
-            wait.Until(waitForElement);
-
-            // Check whether the Booking page is displayed
-            string currentWindowTitle = "All Bookings View";
-            Assert.AreEqual(currentWindowTitle, driver.Title);
-
-            // Find and verify Show entries menu has default value is 10
-            IWebElement showEntries = driver.FindElement(By.XPath("//select[@name='data_length']"));
-            String showDefaultValue = showEntries.GetAttribute("value");
-            Assert.True(!String.IsNullOrEmpty(showDefaultValue));
-            Assert.AreEqual("10", showDefaultValue);
-
-            // Find and verify seach textbox has the default value is empty
-            IWebElement searchBar = driver.FindElement(By.TagName("input"));
-            String searchDefaultValue = showEntries.GetAttribute("placeholder");
-            Assert.True(String.IsNullOrEmpty(searchDefaultValue));
-
-            // Find the data table in booking page and verify it contains the columns
-            driver.Manage().Window.Maximize();
-            IWebElement bookingTable = driver.FindElement(By.XPath("//table[@id='data']"));
-            ReadOnlyCollection<IWebElement> listThElem = bookingTable.FindElements(By.TagName("th"));
-
-            foreach (var elemTh in listThElem)
-            {
-                var thHeader = elemTh.Text;
-                Assert.True(elemTh.Enabled);
-            }
-
         }
 
         // Create a exit process to close the browser whenever all the testing processes is finished
